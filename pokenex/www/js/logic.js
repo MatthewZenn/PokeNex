4124fa8230bbb15d72f7b77783d4c2b54b7a8b5f
Matthew Zenn <spyguy57@gmail.com>
2022-08-18 12:32:30 -0400

    converted to Cordova

--- pokenex/www/js/logic.js
+++ pokenex/www/js/logic.js
@@ -1,10 +1,21 @@
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+}
+
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
+var canvas2 = document.getElementById("canvas3");
+var ctx2 = canvas2.getContext('2d');
 var q = 0;
 var huel = 0;
 var k = 0;
-const colors = ["#d0a8b0", "#787890", "#787890", "black", "white", "white", "white"];
-const colors2 = ["black", "white", "white", "white", "white", "white", "#787890"];
+const colors = ["#f7b58c", "#84739c", "#84739c", "black", "white", "white", "white"];
+const colors2 = ["#f7b58c", "#84739c", "black", "white"];
+const colors3 = ["#586666", "#96adad", "#6c7e7e"];
+const types = ["Poison", "Rock", "Fire", "Water", "Ghost", "Dark", "Fairy", "Grass", "Electric", "Ice", "Normal", "Fighting", "Psychic", "Ice"];
+const symbols = '!_*;$&ABCDEFGHIGJKLMNOPQRSTUVWXYZ';
 
 var matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -41,6 +52,13 @@ var matrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ]
 
+var matrix2 = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+];
+
 document.getElementById("button").onclick = function() {pattern()};
 
 function pattern() {
@@ -68,10 +86,10 @@ function pattern() {
           var rectData2 = {
                 "x": i,
                 "y": j,
-                "width": 3,
-                "height": 3
+                "width": 2,
+                "height": 2
                 };
-          if(p <= 0.01) {
+          if(p <= 0.05) {
             var color2 = colors2[Math.floor(Math.random()*colors.length)];
             ctx.fillStyle = color2;
             ctx.beginPath();
@@ -81,4 +99,59 @@ function pattern() {
           }
         }
     }
+    for(var i = 0; i < matrix.length; i++) {
+      var cube = matrix[i];
+      for(var j = 0; j < cube.length; j++) {
+        var p = Math.random();
+        var rectData = {
+              "x": i,
+              "y": j,
+              "width": 1,
+              "height": 5
+              };
+        if(p <= 0.05) {
+          var color2 = colors2[Math.floor(Math.random()*colors.length)];
+          ctx.fillStyle = color2;
+          ctx.beginPath();
+          ctx.fillRect(rectData.x, rectData.y, rectData.width, rectData.height);
+          ctx.closePath();
+          console.log(color2);
+        }
+      }
+  }
+  ctx2.clearRect(0, 0, canvas.width, canvas.height);
+  for(var y = 0; y < matrix2.length; y++) {
+      var cube2 = matrix2[y];
+      for(var u = 0; u < cube2.length; u++) {
+        var rectData2 = {
+              "x": y,
+              "y": u,
+              "width": 1,
+              "height": 1
+              };
+        var color = colors3[Math.floor(Math.random()*colors3.length)];
+        ctx2.fillStyle = color;
+        ctx2.beginPath();
+        ctx2.fillRect(rectData2.x,rectData2.y, rectData2.width, rectData2.height);
+        ctx2.closePath();
+      }
+  }
+
+  const num = 5;
+  const randomString = (len = 1) => {
+  let randomString = '';
+  for (let i = 0; i < len; i++) {
+    let randomPoz = Math.floor(Math.random() * symbols.length);
+    console.log(symbols.length)
+    randomString += symbols.substring(randomPoz,randomPoz+1);
+  };
+   return randomString;
+};
+
+document.getElementById("name").innerHTML = randomString(num)+"-";
+document.getElementById("type").innerHTML = types[Math.floor(Math.random()*types.length)];
+
+document.getElementById("heal").innerHTML = 'Health: '+Math.floor(Math.random()*100);
+document.getElementById("att").innerHTML = 'Attack: '+Math.floor(Math.random()*100);
+document.getElementById("def").innerHTML = 'Deffense: '+Math.floor(Math.random()*100);
 }
